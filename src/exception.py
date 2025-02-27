7d2404c866c299e02fb3c2ad1285cdc38e2c3c0d
Rahul Singh <being_rahulkumar@Rahuls-MacBook-Pro.local>
2025-02-27 15:23:36 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -14,22 +14,11 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
+        logging.error(self.error_message)
+
     def __str__(self):
         return self.error_message
-    
-if __name__=="__main__":
 
-    logging.basicConfig(
-        filename="error.log",
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        level=logging.ERROR
-    )
-
-    try:
-        1/0
-    except Exception as e:
-        logging.exception("Zero Division Error")
-        raise CustomException(e, sys)
 
 
     
